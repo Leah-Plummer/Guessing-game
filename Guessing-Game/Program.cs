ed5b0a85238c653b664dc2c63336b5b04046a9a6
Leah Plummer <leahplumm@gmail.com>
2022-11-21 10:28:14 -0600

    phases 1-4

--- Guessing-Game/Program.cs
+++ Guessing-Game/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-
-namespace Guessing_Game
+void GuessingGame()
 {
-    class Program
+    pickANumber();
+};
+int theSecretNumber = 42;
+void pickANumber()
+{
+    Console.Write("Guess the number? (1-50):");
+    int userGuess = int.Parse(Console.ReadLine());
+    int i = 1;
+    while (userGuess != theSecretNumber && i < 4)
     {
-        static void Main(string[] args)
+
+
+        if (userGuess != theSecretNumber)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Chance {1-50}");
+            Console.WriteLine("Try again (1-50):");
+            
+            userGuess = int.Parse(Console.ReadLine());
+
         }
+
+
+
+        i++;
+    }
+    if (userGuess == theSecretNumber)
+    {
+        Console.WriteLine("You guessed it!");
+
+    }
+    else
+    {
+        Console.WriteLine("Sorry your wrong");
+
     }
 }
+
+
+
+
+GuessingGame();
+
+
